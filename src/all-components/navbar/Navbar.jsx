68e450880a77db973cc5fa86c9eb57b89f50b856
rakib <kathgolap909@gmail.com>
2024-12-13 16:09:31 +0600

    hero sec done

--- src/all-components/navbar/Navbar.jsx
+++ src/all-components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import navbrandIGM from '../../assets/images/navbrand.png'
+import { Link, useLocation } from 'react-router-dom';
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'HOME', to: '/', current: false },//deafult page
+  { name: 'ABOUT', to: '/about', current: false },
+  { name: 'CONTACT', to: '/contact', current: false },
+  { name: 'TICKET CANCEL', to: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -13,10 +14,11 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+  const location = useLocation()//trac current route location
   return (
-    <Disclosure as="nav" className="bg-gray-800 w-5/6 md:w-3/4 mx-auto rounded-tl-lg rounded-tr-lg absolute top-4 left-1/2 transform -translate-x-1/2">
+    <Disclosure as="nav" className="bg-gray-800 w-[90%] md:w-3/4 mx-auto rounded-tl-lg rounded-tr-lg absolute top-4 left-1/2 transform -translate-x-1/2">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -30,24 +32,26 @@ export default function Navbar() {
             <div className="flex shrink-0 items-center">
               <img
                 alt="Your Company"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
+                src={navbrandIGM}
+                className="h-14 w-auto"
               />
             </div>
-            <div className="hidden sm:ml-6 sm:block">
+            <div className="hidden sm:ml-6 sm:block my-auto">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
+                    to={item.to}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
+                      location.pathname === item.to//active route style
+                        ? 'bg-gray-900 text-white font-bold'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium'
                     )}
                   >
                     {item.name}
-                  </a>
+                  </Link>
+              
                 ))}
               </div>
             </div>
@@ -71,7 +75,7 @@ export default function Navbar() {
                   <img
                     alt=""
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="size-8 rounded-full"
+                    className="size-12 rounded-full"
                   />
                 </MenuButton>
               </div>
